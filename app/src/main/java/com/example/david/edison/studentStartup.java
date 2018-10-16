79d708c6f20e30986d2a158029bfcbcc4c2f1a48
davidbohunovsky <david.bohunovskyy@gmail.com>
2018-10-16 20:42:52 +0200

    Layout 0.1
    Added close to all layouts ( excepts lists )

---
+++ app/src/main/java/com/example/david/edison/studentStartup.java
@@ -0,0 +1,13 @@
+package com.example.david.edison;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class studentStartup extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_student_startup);
+    }
+}
